fa2162fa043f431b3b7b4fefc6deabcf80585968
tal556 <tall5225@gmail.com>
2022-08-25 01:29:05 +0300

    trailer works with modal

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { baseUrl } from "../constants/movie";
 import { Movie } from "../typings";
-import {FaPlay} from 'react-icons/fa';
+import { FaPlay } from "react-icons/fa";
 import { InformationCircleIcon } from "@heroicons/react/solid";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtoms";
 
 interface Props {
   netflixOriginals: Movie[];
 }
 function Banner({ netflixOriginals }: Props) {
   const [movie, setMovie] = useState<Movie | null>(null);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState)
 
   useEffect(() => {
     setMovie(
@@ -27,22 +31,29 @@ function Banner({ netflixOriginals }: Props) {
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
           objectFit="cover"
-         
         />
       </div>
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl ">
         {movie?.title || movie?.name || movie?.original_name}
-        </h1>
+      </h1>
       <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl  ">
         {movie?.overview}
-        </p>
+      </p>
 
-        <div className="flex space-x-3 " >
-          <button className="bannerButton bg-white text-black">
-             <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/> Play
-             </button>
-          <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/></button>
-        </div>
+      <div className="flex space-x-3 ">
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button
+          className="bannerButton bg-[gray]/70"
+          onClick={() => {
+            setCurrentMovie(movie);
+            setShowModal(true);
+          }}
+        >
+          More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
+      </div>
     </div>
   );
 }
